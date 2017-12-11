9833cf02b53ae02382054d35b2ae74d337b34365
zizhong <zizhong@Lenovo-PC>
2017-12-12 07:09:04 +0800

    fpgrowth 0.01

--- src/main/java/FPgrowth/FPGrowth.java
+++ src/main/java/FPgrowth/FPGrowth.java
@@ -1,6 +1,19 @@
 package FPgrowth;
 
+import java.util.List;
+
+import util.MD5Util;
+
 public class FPGrowth {
 	
+	public static <T> String frozenset(List<T> dataList) {
+		String md5;
+		String orginVal = "";
+		for(int i=0;i<dataList.size();i++){
+			T key = dataList.get(i);
+			orginVal = orginVal + key.toString() + "\r\n";
+		}	
+		return MD5Util.MD5(orginVal);
+	}
 	
 }
--- src/test/java/FPgrowth/TestFPGrowth.java
+++ src/test/java/FPgrowth/TestFPGrowth.java
@@ -1,5 +1,33 @@
 package FPgrowth;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class TestFPGrowth {
 
+	public static List<String> textParse(String bigString) {
+		String urlRemoved = bigString.replaceAll("(http[s]?:[/][/]|www.)([a-z]|[A-Z]|[0-9]|[/.]|[~])*", "");
+		String[] listOfTokens = urlRemoved.split("\\W");
+		List<String> tokList = new ArrayList<String>();
+		for(int i=0;i<listOfTokens.length;i++) {
+			if(listOfTokens[i].length() > 2) {
+				tokList.add(listOfTokens[i]);
+			}
+		}
+		return tokList;
+	}
+	
+	public static void loadDataSet() {
+		Map<String,Integer> retDict = new HashMap<String,Integer>(); 
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(FPGrowth.frozenset(textParse("This random girl is arguing the nba needs to raise the rims to make it more interesting... https://t.co/AGd1IrUc13")));
+		
+		// textParse("This random girl is arguing the nba needs to raise the rims to make it more interesting... https://t.co/AGd1IrUc13");
+	}
 }
